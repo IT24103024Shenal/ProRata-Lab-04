79d25293eb994f4d7dca79be2cb1f6af0ba68242
Ramidu Shenal <it24103024@my.sliit.lk>
2026-09-17 11:54:58 +0530

    Add files via upload

---
+++ Q3/IT24103024Lab4Q3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class IT24103024Lab4Q3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = input.nextDouble();
+
+        String result = (number > 0) ? "Positive" : (number < 0) ? "Negative" : "Zero";
+
+        System.out.println("The number is: " + result);
+    }
+}
